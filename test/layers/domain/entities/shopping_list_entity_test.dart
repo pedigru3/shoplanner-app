a83f4685356af01eb1660fa83acc56f6226df5b1
pedigru3 <contosdeferreira@hotmail.com>
2023-06-12 08:10:07 -0300

    delete test folder

--- test/layers/domain/entities/shopping_list_entity_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:shoplanner/layers/domain/entities/shopping_list_entity.dart';
-
-void main() {
-  test('deve retornar nome do supermercado', () {
-    ShoppingListEntity shop = ShoppingListEntity(name: 'Mufato');
-    expect(shop.name, 'Mufato');
-  });
-}
